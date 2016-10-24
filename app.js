eb37d6aa56edc04723209907314f4b5ef8d6f903
Hrishikesh Ballal <hballal@gmail.com>
2016-10-24 22:43:09 +0100

    updated to fix url

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/', function(request, response) {
             var gj = JSON.stringify(results[0]['geojson']);
             var bounds = results[1];
 
-            var roadsURL = "https://geodzn.com/api/v1/sql/gdhsupport?q=SELECT ST_AsGeoJSON(threedviewer.roadsall.the_geom) FROM threedviewer.roadsall WHERE threedviewer.roadsall.the_geom @ ST_MakeEnvelope(" + bounds['bounds'] + ")&key=54ed6c30bec7a53df8202d6057806a03"
+            var roadsURL = "https://geodzn.com/api/v1/sql/gdhsupport?q=SELECT ST_AsGeoJSON(threedviewer.roadsall.the_geom) FROM threedviewer.roadsall WHERE threedviewer.roadsall.the_geom @ ST_MakeEnvelope(" + bounds['bounds'] + ")&key=54ed6c30bec7a53df8202d6057806a03";
 
             var rURls = [roadsURL];
 
@@ -69,10 +69,10 @@ app.get('/', function(request, response) {
                 if (err) return response.sendStatus(500);
                 opts['result'] = gj;
                 var rfc1 = { "type": "FeatureCollection", "features": [] };
-
-                var rlen1 = roads.features.length;
+                console.log(roads);
+                var rlen1 = roads[0].features.length;
                 for (var x7 = 0; x7 < rlen1; x7++) {
-                    var curroad = roads.features[x7];
+                    var curroad = roads[0].features[x7];
                     var roadgj = JSON.parse(curroad.properties.st_asgeojson);
                     var f8 = { "type": "Feature", "properties": {}, "geometry": roadgj };
                     rfc1.features.push(f8);
@@ -87,8 +87,8 @@ app.get('/', function(request, response) {
     } else if (request.query.apitoken && request.query.projectid && request.query.synthesisid && request.query.cteamid) {
         opts = { 'apitoken': request.query.apitoken, 'projectid': request.query.projectid, 'synthesisid': request.query.synthesisid, 'cteamid': request.query.cteamid, 'diagramid': '0' };
 
-        var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
-        // var baseurl = 'http://local.dev:8000/api/v1/projects/';
+        // var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
+        var baseurl = 'http://local.dev:8000/api/v1/projects/';
 
         var apikey = request.query.apitoken;
         var cred = "Token " + apikey;
@@ -98,7 +98,7 @@ app.get('/', function(request, response) {
         var synprojectsurl = baseurl + projectid + '/cteams/' + cteamid + '/' + synthesisid + '/';
         var boundsurl = baseurl + projectid + '/bounds/';
         var URLS = [synprojectsurl, boundsurl];
-
+        console.log(synprojectsurl);
         async.map(URLS, function(url, done) {
             req({
                 url: url,
@@ -118,7 +118,7 @@ app.get('/', function(request, response) {
             var gj = JSON.stringify(results[0]);
             var bounds = results[1];
 
-            var roadsURL = "https://geodzn.com/api/v1/sql/gdhsupport?q=SELECT ST_AsGeoJSON(threedviewer.roadsall.the_geom) FROM threedviewer.roadsall WHERE threedviewer.roadsall.the_geom @ ST_MakeEnvelope(" + bounds['bounds'] + ")&key=54ed6c30bec7a53df8202d6057806a03"
+            var roadsURL = "https://geodzn.com/api/v1/sql/gdhsupport?q=SELECT ST_AsGeoJSON(threedviewer.roadsall.the_geom) FROM threedviewer.roadsall WHERE threedviewer.roadsall.the_geom @ ST_MakeEnvelope(" + bounds['bounds'] + ")&key=54ed6c30bec7a53df8202d6057806a03";
 
             var rURls = [roadsURL];
             // console.log(roadsURL);
@@ -136,12 +136,13 @@ app.get('/', function(request, response) {
                 });
             }, function(err, roads) {
 
+
                 if (err) return response.sendStatus(500);
                 opts['result'] = gj;
                 var rfc = { "type": "FeatureCollection", "features": [] };
-                var rlen = roads.features.length;
+                var rlen = roads[0].features.length;
                 for (var x5 = 0; x5 < rlen; x5++) {
-                    var curroad = roads.features[x5];
+                    var curroad = roads[0].features[x5];
                     var roadgj = JSON.parse(curroad.properties.st_asgeojson);
                     var f = { "type": "Feature", "properties": {}, "geometry": roadgj };
                     rfc.features.push(f);
