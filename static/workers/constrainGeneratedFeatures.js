61e96a598f2afb57fe9e26afc889594ee41bcc27
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2016-11-02 14:51:28 +0000

    Added cosntrain worker thread

---
+++ static/workers/constrainGeneratedFeatures.js
@@ -0,0 +1,33 @@
+
+function constrainFeatures(allFeaturesList, selectedsystems) {
+    // constrain output ot only features in the list. 
+    var constraintedFeatures = { "type": "FeatureCollection", "features": [] };
+    var allFeatures = JSON.parse(allFeaturesList);
+
+    var selectedsystems = JSON.parse(selectedsystems);
+    var af = allFeatures.features;
+    var featlen = af.length;
+    var counter = 0;
+    var fullproc = featlen;
+    for (var d = 0; d < featlen; d++) {
+        var curfeatprop = af[d].properties;
+        var curFeatSys = curfeatprop.sysname;
+        if (selectedsystems.indexOf(curFeatSys) > -1) {
+            constraintedFeatures.features.push(af[d]);
+        }
+        counter += 1;
+
+        self.postMessage({
+            'percentcomplete': parseInt((100 * counter) / fullproc),
+            'mode': 'status',
+        });
+    }
+
+    self.postMessage({
+        'polygons': JSON.stringify(constraintedFeatures)
+    });
+}
+
+self.onmessage = function (e) {
+    constrainFeatures(e.data.allFeaturesList, e.data.selectedsystems);
+}
